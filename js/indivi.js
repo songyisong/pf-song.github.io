56c8cd0626bbdc8677776b4191d00729479c3859
songyisong <59271078+songyisong@users.noreply.github.com>
2021-01-18 16:36:14 +0900

    js 수정

--- js/indivi.js
+++ js/indivi.js
@@ -22,10 +22,33 @@ mTxt.from(".section .intro_txt .char",{
     stagger:0.1
 })
 
-// 한줄로 표기
-// gsap.from("#mainVisual .slogan .sub .word",{x:100, opacity:0, duration:1, ease:"power3", stagger:0.1, delay:0.5});
+new fullpage("#fullpage",{
+    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor: ['#fff', '#fff', '#fff', 'transparent'],
+    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
+    menu:'#menu',
+    scrollingSpeed:1000    
+});
 
+$(".intro_white_wrap").click(function(){
+    $(this).addClass("disable");
+});
 
+//menu
+$(".logo > a").click(function() {
+    if($("nav").hasClass("on")){ // nav실행
+        $(this).parent().siblings("nav").removeClass("on");
+    } else { //nav닫힘
+        $(this).parent().siblings("nav").addClass("on");
+    }
+});
+$('#menu > a').on('click', function() {
+    $('#menu').removeClass('on');
+});
+
+//work swiper 
 var swiper = new Swiper('.swiper-container', {
     speed: 1000,
     autoplay: {
@@ -38,32 +61,4 @@ var swiper = new Swiper('.swiper-container', {
     clickable: true,
     },
 
-});
-
-$(".intro_white_wrap").click(function(){
-    $(this).addClass("disable");
-});
-$(".logo > a").click(function() {
-    if($("nav").hasClass("on")){ // nav실행
-        $(this).parent().siblings("nav").removeClass("on");
-    } else { //nav닫힘
-        $(this).parent().siblings("nav").addClass("on");
-    }
-});
-
-new fullpage("#fullpage",{
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    autoScrolling:true,
-    scrollHorizontally: true,
-    sectionsColor: ['#fff', '#fff', '#fff', 'transparent'],
-    anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage'],
-    menu:'#menu',
-    scrollingSpeed:1000    
-});
-
-$('.p_list').mouseover(function (){
-    $(this).addClass("on");
-});
-$('.p_list').mouseleave(function (){
-    $(this).removeClass("on");
 });
